d1de61ba8de9e4d30f6a42cd0ca6e7a260c11e80
MD AL JABIR <aljabir098@gmail.com>
2022-09-14 19:10:26 +0600

    initial

--- src/Components/Users/AddUsers/AddUser.jsx
+++ src/Components/Users/AddUsers/AddUser.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../UI/Cards/Card";
 
 const AddUser = () => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+
+  const userAgeHangler = (e) => {
+    setUserAge(e.target.vale);
+  };
+  const userNameHandler = (e) => {
+    setUserName(e.target.value);
+  };
   return (
     <div>
       <Card>
         <form>
           <label htmlFor="username">Username</label>
-          <input type="text" />
+          <input type="text" value={userName} onChange={userNameHandler} />
           <label htmlFor="age">Age</label>
-          <input type="number" />
+          <input type="number" value={userAge} onChange={userAgeHangler} />
           <button type="submit">Add User</button>
         </form>
       </Card>
