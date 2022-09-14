22d72114f06993b8a08cb8b548c60a7fd3647dd8
MD AL JABIR <aljabir098@gmail.com>
2022-09-14 21:26:06 +0600

    some add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./Components/Users/AddUsers/AddUser";
+import UserList from "./Components/Users/UserLists/UserList";
 
 const App = () => {
+  const [userList, setUserList] = useState([]);
+
+  const itemListHandle = (name, age) => {
+    setUserList((prevList) => {
+      return [...prevList, { name: name, age: age }];
+    });
+  };
+
   return (
     <>
-      <AddUser />
+      <AddUser onAddUserHandle={itemListHandle} />
+      <UserList item={userList} />
     </>
   );
 };
